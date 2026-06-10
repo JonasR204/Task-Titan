bc2476af06797070a6a1255f11e6736f0134d239
ecpaksoy <ecanpaksoy2002@gmail.com>
2026-06-10 19:10:00 +0200

    Add files via upload

---
+++ Database.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/tasktitan_app";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "hello"; //replace with your mysql password if applicable
+
+    public static Connection getConnection()
+            throws SQLException {
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
