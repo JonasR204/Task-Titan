d8ccecc0bb447201b72b844cc36a45c0c0633139
Mohammed Al-areqi <moh199877@gmail.com>
2026-06-27 21:46:14 +0200

    BugTask.java

---
+++ tasktitanpackages/model/BugTask.java
@@ -0,0 +1,33 @@
+package model;
+
+import model.Priority;
+import model.TaskStatus;
+import model.TaskType;
+import java.time.LocalDate;
+
+public class BugTask extends Task {
+    private String codePart;
+    private String errorMessage;
+    private String fixProposition;
+
+
+    public BugTask(int id, String title, String description, Priority priority,
+            TaskStatus status, LocalDate deadline, TeamMember assignedMEmber,
+            String codePart, String errorMessage, String fixProposition){
+        super(id, title, description, priority, status, deadline, assignedMEmber);
+
+        taskType = TaskType.BUG;
+        this.codePart = codePart;
+        this.errorMessage = errorMessage;
+        this.fixProposition = fixProposition;
+    }
+
+    public String getCodePart() {return codePart;}
+    public void SetCodePart(String codePart) {this.codePart = codePart;}
+
+    public String getErrorMessage() {return errorMessage;}
+    public void SetErrorMessage(String errorMessage) {this.errorMessage = errorMessage;}
+
+    public String getFixProposition() {return fixProposition;}
+    public void SetFixProposition(String fixProposition) {this.fixProposition = fixProposition;}
+}
